6a4dc74aee040bb71e6fa1fdaf0873c5d6d114b5
lenargaynullin <lenar.gaynullin88@gmail.com>
2022-02-13 13:06:14 +0300

    13.02.2022

--- DemoApiTesting/contracts/GetPlanetsTests.cs
+++ DemoApiTesting/contracts/GetPlanetsTests.cs
@@ -22,15 +22,19 @@ namespace DemoApiTesting
         
 
         [Test]
-        [TestCase(0)]
+        [TestCase(9999)]
         
+       
         public async Task CheckContractPlanetsApiTesting(int page)
         {
             string Api = $"/planets/?page={page}";
             var baseAddress = new Uri(Host + Api);
-            var client = new HttpClient() { BaseAddress = baseAddress } ;
+            var client = new HttpClient() { BaseAddress = baseAddress }; // https://swapi.dev/api/planets/?page=
+
+            // Ответ от сервера
             var response = await client.GetAsync(baseAddress, new CancellationToken());
 
+            // Строковый ответ от сервера 
             var stringResponse = await response.Content.ReadAsStringAsync();
             
             // преобразуем содержимое ответа апи в строку
@@ -43,23 +47,21 @@ namespace DemoApiTesting
             var direct = Directory.GetCurrentDirectory();
             
             // Убираем из этого пути лишнее (все,что после bin) и заменяем его на relativePath
-            var path = direct.Substring(0,
-                direct.IndexOf(@"\bin\", StringComparison.Ordinal)) + @"\contracts\";
-            /*
-             Преобразуем наш файл getUsers.Positive.json в формат JSchema (для этого мы сначала считаем файл в строку)
-             Документацию по JSON Schema Validation
-             можно посмотреть тут - https://json-schema.org/draft/2019-09/json-schema-validation.html
-              */
-            JSchema schema = JSchema.Parse(File.ReadAllText($@"{path}"+"getPlanets.Positive.json"));
-             // преобразуем стринговый ответ от апи в JObject
-             var jObject = JObject.Parse(stringResponse);
-             if (jObject == null) throw new ArgumentNullException(nameof(jObject));
+            var path = direct.Substring(0, direct.IndexOf(@"\bin\", StringComparison.Ordinal)) + @"\contracts\";
+                        
+            // Преобразуем наш файл getUsers.Negative.json в формат JSchema (для этого мы сначала считаем файл в строку)
+            // Документацию по JSON Schema Validation можно посмотреть тут:
+            // https://json-schema.org/draft/2019-09/json-schema-validation.html
+            JSchema schema = JSchema.Parse(File.ReadAllText($@"{path}"+"getPlanets.Negative.json"));
+
+            // преобразуем стринговый ответ от апи в JObject
+            var jObject = JObject.Parse(stringResponse);
+            if (jObject == null) throw new ArgumentNullException(nameof(jObject));
              
-             // метод IsValid проверяет соответствует ли ответ апи (jObject) нашему контрактному файлу getUsers.Positive.json (schema)
+            // метод IsValid проверяет соответствует ли ответ апи (jObject) нашему контрактному файлу getUsers.Negative.json (schema)
             var valid = jObject.IsValid(schema, out IList<string> messages);
             
-            Assert.IsTrue(valid,
-                $"Полученный json невалиден. Невалидные поля {string.Join(", ", messages.ToArray())}");
+            Assert.IsTrue(valid, $"Полученный json невалиден. Невалидные поля {string.Join(", ", messages.ToArray())}");
         }
         
     }
